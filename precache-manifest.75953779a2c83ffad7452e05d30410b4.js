c5bbbe0b604195ee60b41c69025a56ec0c0c6874
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-09-20 12:17:08 -0600

    Updates

---
+++ precache-manifest.75953779a2c83ffad7452e05d30410b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eef03c5a9f593dc24e348017087ff3d",
+    "url": "/WKRBee/index.html"
+  },
+  {
+    "revision": "29bd3c009bf3f5979b45",
+    "url": "/WKRBee/static/css/main.beddca8f.chunk.css"
+  },
+  {
+    "revision": "603f36e589afb1a4de3b",
+    "url": "/WKRBee/static/js/2.1105958e.chunk.js"
+  },
+  {
+    "revision": "29bd3c009bf3f5979b45",
+    "url": "/WKRBee/static/js/main.c6cc4d44.chunk.js"
+  },
+  {
+    "revision": "af496114f329ea26a855",
+    "url": "/WKRBee/static/js/runtime~main.b98a126f.js"
+  },
+  {
+    "revision": "bd81c4344e429a1cac618e178712c23a",
+    "url": "/WKRBee/static/media/beeLogo_large.bd81c434.png"
+  },
+  {
+    "revision": "09f2e51e4b2822f27f84885042af07e2",
+    "url": "/WKRBee/static/media/beehive.09f2e51e.png"
+  },
+  {
+    "revision": "20b734d6a1efba4f488b26d3f88d4353",
+    "url": "/WKRBee/static/media/honeycomb_light.20b734d6.jpg"
+  }
+]);
